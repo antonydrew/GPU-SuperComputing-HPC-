0920a1d051d87f4d9767c2278fc2f09e275e6db9
Antony Drew <adrew@luc.edu>
2013-02-07 02:59:35 -0600

    LINUX version of CUDA C GPU Demo - you will need to install CUDA toolkit on Lin

---
+++ DemoCu/Debug/src/subdir.mk
@@ -0,0 +1,25 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CU_SRCS += \
+../src/DemoCu.cu 
+
+CU_DEPS += \
+./src/DemoCu.d 
+
+OBJS += \
+./src/DemoCu.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cu
+	@echo 'Building file: $<'
+	@echo 'Invoking: NVCC Compiler'
+	nvcc -G -g -O0 -gencode arch=compute_30,code=sm_30 -odir "src" -M -o "$(@:%.o=%.d)" "$<"
+	nvcc --compile -G -O0 -g -gencode arch=compute_30,code=compute_30 -gencode arch=compute_30,code=sm_30  -x cu -o  "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
